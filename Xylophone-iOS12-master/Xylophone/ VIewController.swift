e9fba6d2fb18ebe542c553033998636eb62944d9
Zumhliansang Lungler <Zlungler5069@conestogac.on.ca>
2018-09-28 17:27:19 -0400

    Finished Xylophone

--- Xylophone-iOS12-master/Xylophone/ VIewController.swift
+++ Xylophone-iOS12-master/Xylophone/ VIewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer : AVAudioPlayer!
+    let sounds = ["note1","note2","note3","note4","note5","note6","note7"]
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,8 +21,24 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        soundPlay(notes: sender.tag - 1)
         
+    }
+    
+    func soundPlay(notes: Int){
+        /*
+         Here we learn about to set URL for the sound
+         */
         
+        let soundURL = Bundle.main.url(forResource: sounds[notes], withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
     }
     
   
