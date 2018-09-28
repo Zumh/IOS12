991e05c1ca99a1373cba7e8d9fd49672a2e313a4
Zumhliansang Lungler <Zlungler5069@conestogac.on.ca>
2018-09-28 14:46:17 -0400

    Swift basic

---
+++ SwiftBasics/DataTypes,ConstantsAndVariables.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// we can change the value
+var myAge : Int = 32
+
+myAge = 22
+
+//print(myAge)
+
+// let is a constant variable, less memory
+let myName : String = "Angela"
+//myName = "Jame"
+
+let myAgeInTenYears = myAge + 10
+
+//let myFullName = myName + " james"
+let myFullName = "\(myName)"
+
+// , allow us to convert int to string
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+let text : String = "abc"
+let boolean : Bool = true
+let floatingPointNumber : Float = 1.3
+let double : Double = 3.14159265359
+
+
+
+
+
